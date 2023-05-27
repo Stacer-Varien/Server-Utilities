6fd05baf42bf21598b625ea7c26045e0080365fa
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-05-28 00:35:26 +0200

    Improved break updater

--- LOA/break_updater.py
+++ LOA/break_updater.py
@@ -4,7 +4,7 @@ from discord.ext import tasks
 from discord.ext.commands import Cog, Bot
 
 
-class break_updater(Cog):
+class BreakUpdater(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
         self.check_break.start()
@@ -12,25 +12,23 @@ class break_updater(Cog):
     @tasks.loop(minutes=5)
     async def check_break(self):
         check_breaks = Break().check_breaks()
-        loa = await self.bot.fetch_guild(841671029066956831)
+        loa = self.bot.get_guild(841671029066956831)  # Use get_guild instead of fetch_guild if possible
         break_role = loa.get_role(841682795277713498)
-        break_channel = await loa.fetch_channel(841676953613631499)
+        break_channel = self.bot.get_channel(841676953613631499)  # Use get_channel instead of fetch_channel if possible
 
         for a in check_breaks:
             try:
                 if int(round(datetime.now().timestamp())) > int(a[6]):
-                    member = await loa.fetch_member(a[0])
+                    member = loa.get_member(a[0])  # Use get_member instead of fetch_member if possible
 
                     await member.remove_roles(break_role, reason="Break has ended")
 
                     Break(member).remove()
 
-                    await break_channel.send(
-                        "{}, your break has ended".format(member.mention)
-                    )
-            except:
+                    await break_channel.send(f"{member.mention}, your break has ended")
+            except Exception:
                 continue
 
 
 async def setup(bot: Bot):
-    await bot.add_cog(break_updater(bot))
+    await bot.add_cog(BreakUpdater(bot))
