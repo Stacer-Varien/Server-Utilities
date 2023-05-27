a317cca76e55bdefb8b3c398f331a77bbe05fc58
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-05-28 00:35:59 +0200

    Update server_utilities.py

--- server_utilities.py
+++ server_utilities.py
@@ -1,7 +1,9 @@
-from os import listdir
-from discord import Intents, Object
+import os
+from discord import Intents
 from discord.ext.commands import Bot, when_mentioned_or
+from discord.ext import commands
 from config import TOKEN, lss, loa, orleans, hazead
+
 
 intents = Intents().all()
 intents.presences = False
@@ -11,55 +13,55 @@ intents.guild_scheduled_events = False
 
 
 class ServerUtilities(Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.remove_command("help")
+
+    async def setup_extensions(self, path, package):
+        for filename in os.listdir(path):
+            if filename.endswith(".py"):
+                try:
+                    self.load_extension(f"{package}.{filename[:-3]}")
+                    print(f"{filename} loaded")
+                except commands.ExtensionError as e:
+                    print(f"Failed to load {filename[:-3]}: {e}")
+            else:
+                print(f"Unable to load {filename[:-3]}")
+
+    async def on_ready(self):
+        print("Connected to bot: {}".format(self.user.name))
+        print("Bot ID: {}".format(self.user.id))
+        for guild_id in [lss, loa, orleans, hazead]:
+            guild = self.get_guild(guild_id)
+            if guild is not None:
+                await self.tree.sync(guild=guild)
+
     async def setup_hook(self):
-        await bot.load_extension("jishaku")
-        for filename in listdir("./HA"):
-            if filename.endswith(".py"):
-                await bot.load_extension(f"HA.{filename[:-3]}")
-                print(f"{filename} loaded")
-            else:
-                print(f"Unable to load {filename[:-3]}")
-
-        for filename in listdir("./admod"):
-            if filename.endswith(".py"):
-                await bot.load_extension(f"admod.{filename[:-3]}")
-                print(f"{filename} loaded")
-            else:
-                print(f"Unable to load {filename[:-3]}")
-
-        for filename in listdir("./LOA"):
-            if filename.endswith(".py"):
-                await bot.load_extension(f"LOA.{filename[:-3]}")
-                print(f"{filename} loaded")
-            else:
-                print(f"Unable to load {filename[:-3]}")
-
-        for filename in listdir("./shared"):
-            if filename.endswith(".py"):
-                await bot.load_extension(f"shared.{filename[:-3]}")
-                print(f"{filename} loaded")
-            else:
-                print(f"Unable to load {filename[:-3]}")
+        await self.setup_extensions("./HA", "HA")
+        await self.setup_extensions("./admod", "admod")
+        await self.setup_extensions("./LOA", "LOA")
+        await self.setup_extensions("./shared", "shared")
 
 
 bot = ServerUtilities(
     intents=intents,
     command_prefix=when_mentioned_or("su!", "SU!", "Su!", "su", "SU", "sU"),
 )
-bot.remove_command("help")
 
 
 @bot.event
-async def on_ready():
-    print("Connected to bot: {}".format(bot.user.name))
-    print("Bot ID: {}".format(bot.user.id))
-    for guild in [
-        Object(id=lss),
-        Object(id=loa),
-        Object(id=orleans),
-        Object(id=hazead),
-    ]:
-        await bot.tree.sync(guild=guild)
+async def on_connect():
+    print("Bot connected to Discord")
+
+
+@bot.event
+async def on_disconnect():
+    print("Bot disconnected from Discord")
+
+
+@bot.event
+async def on_resumed():
+    print("Bot resumed")
 
 
 bot.run(TOKEN)
