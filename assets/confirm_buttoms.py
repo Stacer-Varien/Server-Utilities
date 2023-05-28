744abc66428e9860f13fe0514307e41ddf0cecac
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-05-28 23:33:38 +0200

    Added user lock check

--- assets/confirm_buttoms.py
+++ assets/confirm_buttoms.py
@@ -1,10 +1,12 @@
-from discord import ui, ButtonStyle, Interaction
+from discord import User, ui, ButtonStyle, Interaction
+from discord.interactions import Interaction
 
 
 class Confirmation(ui.View):
-    def __init__(self):
+    def __init__(self, author:User):
         super().__init__(timeout=600)
         self.value = None
+        self.author=author
 
     @ui.button(label="Confirm", style=ButtonStyle.green)
     async def confirm(self, button: ui.Button, ctx: Interaction):
@@ -15,3 +17,6 @@ class Confirmation(ui.View):
     async def cancel(self, button: ui.Button, ctx: Interaction):
         self.value = False
         self.stop()
+    
+    async def interaction_check(self, ctx: Interaction):
+        return ctx.user.id == self.author.id
