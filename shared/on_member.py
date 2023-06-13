947297f1ecd6491c635cfb4932012d4da34889f0
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-06-13 14:41:27 +0200

    Update on_member.py

--- shared/on_member.py
+++ shared/on_member.py
@@ -9,7 +9,6 @@ HAZE_ADS = 925790259160166460
 
 
 class member(Cog):
-
     def __init__(self, bot: Bot):
         self.bot = bot
 
@@ -18,7 +17,7 @@ class member(Cog):
         if member.guild.id == HAZE_ADS:
             channel = self.bot.get_channel(WELCOME_CHANNEL_ID)
             try:
-                welcome = f"{member.mention}\n\nHello and welcome to {member.guild.name}!\nBefore you can start advertising and chatting, please read the <#925790259877412876> and <#925790259877412883> so you don't get in trouble. Now please, enjoy your stay and start advertising yourself!\n\n **__Special Servers:__**\n1. Lead of Advertising: https://discord.gg/gpDcZfF\n2. Semi-Erasor https://discord.gg/VhgWsfN8ku\n3. Orleans https://discord.gg/Vfa796yvNq\n4. Safe Space https://discord.gg/HXQFSfZfws"
+                welcome = f"{member.mention}\n\nHello and welcome to {member.guild.name}!\nBefore you can start advertising and chatting, please read the <#925790259877412876> and <#925790259877412883> so you don't get in trouble. Now please, enjoy your stay and start advertising yourself!\n\n **__Special Servers:__**\n1. Lead of Advertising: https://discord.gg/gpDcZfF\n2. Semi-Erasor https://discord.gg/VhgWsfN8ku\n3. Orleans https://discord.gg/Vfa796yvNq"
 
                 embed = Embed(colour=Colour.blue())
                 if member.avatar != None:
@@ -26,32 +25,33 @@ class member(Cog):
                 else:
                     pass
                 embed.set_image(
-                    url=
-                    "https://media.discordapp.net/attachments/829041410556690452/951799345324376065/swim-ad.gif"
+                    url="https://media.discordapp.net/attachments/829041410556690452/951799345324376065/swim-ad.gif"
                 )
                 await channel.send(welcome, embed=embed)
-            except Exception as e:
-                raise e
+            except:
+                pass
         else:
             pass
 
     @Cog.listener()
     async def on_member_remove(self, member: Member):
-        if member.guild.id == HAZE_ADS:
-            channel = self.bot.get_channel(WELCOME_CHANNEL_ID)
-            try:
-                bye = f"{member} left us."
-                await channel.send(bye)
-            except:
-                pass
+        try:
+            if member.guild.id == HAZE_ADS:
+                channel = self.bot.get_channel(WELCOME_CHANNEL_ID)
+                try:
+                    bye = f"{member} left us."
+                    await channel.send(bye)
+                except:
+                    pass
 
-        elif member.guild.id == 841671029066956831:
-            channel = self.bot.get_channel(841672222136991757)
-            try:
-                await Resign(member).resigned(channel)
-            except:
-                pass
-
+            elif member.guild.id == 841671029066956831:
+                channel = self.bot.get_channel(841672222136991757)
+                try:
+                    await Resign(member).resigned(channel)
+                except:
+                    pass
+        except:
+            pass
 
 
 async def setup(bot: Bot):
