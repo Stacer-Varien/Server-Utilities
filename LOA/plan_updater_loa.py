bf3e1003c11bed3e251770c7b7d75ecf9089970f
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-06-14 16:09:45 +0200

    Improved LOA's plan updater

--- LOA/plan_updater_loa.py
+++ LOA/plan_updater_loa.py
@@ -1,42 +1,40 @@
 from discord import Embed
-from discord.ext import tasks
-from discord.ext.commands import Cog, Bot
+from discord.ext import tasks, commands
 from datetime import datetime
 from assets.functions import Plans
 
 
-class PlanUpdaterCogLoa(Cog):
-    def __init__(self, bot: Bot):
+class PlanUpdaterCogLoa(commands.Cog):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.update_plans_loa.start()
 
     @tasks.loop(minutes=5)
     async def update_plans_loa(self):
-        loa_plan_log = await self.bot.fetch_channel(990246941029990420)
+        loa_plan_log = self.bot.get_channel(990246941029990420)
 
         plans = Plans(704888699590279221)
 
         if plans.check() is None:
-            pass
+            return
         else:
             for i in plans.check():
                 buyer = await self.bot.fetch_user(int(i[0]))
-                ending = i[2]
+                ending = int(i[2])
                 plan = i[3]
                 setter = await self.bot.fetch_user(int(i[4]))
-                plan_id = i[5]
+                plan_id = int(i[5])
 
-                if int(round(datetime.now().timestamp())) > int(ending):
+                if int(datetime.now().timestamp()) > ending:
                     embed = Embed(title="Plan")
                     embed.add_field(name="Buyer", value=buyer, inline=False)
                     embed.add_field(name="Product", value=plan, inline=False)
                     await loa_plan_log.send(
                         "{}, {} has ended".format(setter.mention, plan_id), embed=embed
                     )
-                    plans.remove(buyer, int(plan_id))
+                    plans.remove(buyer, plan_id)
                 else:
                     pass
 
-
-async def setup(bot: Bot):
+async def setup(bot: commands.Bot):
     await bot.add_cog(PlanUpdaterCogLoa(bot))
