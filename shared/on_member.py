35560f541c6612845e032bbe05d7a5ef412f184d
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-06-16 23:44:40 +0200

    Update on_member.py

--- shared/on_member.py
+++ shared/on_member.py
@@ -1,58 +1,63 @@
-from discord import *
-from discord.ext.commands import Cog, Bot
+from discord import Embed, Colour, Member
+from discord.ext.commands import Cog
+
 from assets.functions import Resign
 
-from config import db
 
 WELCOME_CHANNEL_ID = 925790259877412877
 HAZE_ADS = 925790259160166460
 
 
-class member(Cog):
-    def __init__(self, bot: Bot):
+class MemberCog(Cog):
+    def __init__(self, bot):
         self.bot = bot
 
     @Cog.listener()
-    async def on_member_join(self, member: Member):
+    async def on_member_join(self, member:Member):
         if member.guild.id == HAZE_ADS:
             channel = self.bot.get_channel(WELCOME_CHANNEL_ID)
-            try:
-                welcome = f"{member.mention}\n\nHello and welcome to {member.guild.name}!\nBefore you can start advertising and chatting, please read the <#925790259877412876> and <#925790259877412883> so you don't get in trouble. Now please, enjoy your stay and start advertising yourself!\n\n **__Special Servers:__**\n1. Lead of Advertising: https://discord.gg/gpDcZfF\n2. Semi-Erasor https://discord.gg/VhgWsfN8ku\n3. Orleans https://discord.gg/Vfa796yvNq"
+            welcome = (
+                f"{member.mention}\n\nHello and welcome to {member.guild.name}!\n"
+                "Before you can start advertising and chatting, please read the "
+                "<#925790259877412876> and <#925790259877412883> so you don't get in trouble."
+                " Now please, enjoy your stay and start advertising yourself!\n\n"
+                " **__Special Servers:__**\n"
+                "1. Lead of Advertising: https://discord.gg/gpDcZfF\n"
+                "2. Semi-Erasor https://discord.gg/VhgWsfN8ku\n"
+                "3. Orleans https://discord.gg/Vfa796yvNq"
+            )
 
-                embed = Embed(colour=Colour.blue())
-                if member.avatar != None:
-                    embed.set_thumbnail(url=member.avatar)
-                else:
-                    pass
-                embed.set_image(
-                    url="https://media.discordapp.net/attachments/829041410556690452/951799345324376065/swim-ad.gif"
-                )
+            embed = Embed(colour=Colour.blue())
+            if member.avatar:
+                embed.set_thumbnail(url=member.avatar.url)
+            embed.set_image(
+                url="https://media.discordapp.net/attachments/829041410556690452/951799345324376065/swim-ad.gif"
+            )
+
+            try:
                 await channel.send(welcome, embed=embed)
-            except:
-                pass
-        else:
-            pass
+            except Exception as e:
+                print(f"Failed to send welcome message: {e}")
 
     @Cog.listener()
-    async def on_member_remove(self, member: Member):
+    async def on_member_remove(self, member):
         try:
             if member.guild.id == HAZE_ADS:
                 channel = self.bot.get_channel(WELCOME_CHANNEL_ID)
+                bye = f"{member} left us."
                 try:
-                    bye = f"{member} left us."
                     await channel.send(bye)
-                except:
-                    pass
+                except Exception as e:
+                    print(f"Failed to send goodbye message: {e}")
 
             elif member.guild.id == 841671029066956831:
                 channel = self.bot.get_channel(841672222136991757)
                 try:
                     await Resign(member).resigned(channel)
-                except:
-                    pass
-        except:
-            pass
+                except Exception as e:
+                    print(f"Failed to send resign message: {e}")
+        except Exception as e:
+            print(f"Error: {e}")
 
-
-async def setup(bot: Bot):
-    await bot.add_cog(member(bot))
+async def setup(bot):
+    await bot.add_cog(MemberCog(bot))
