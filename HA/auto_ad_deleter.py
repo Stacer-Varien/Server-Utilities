6a6890e91956b9de7a4985707dd25497d317172f
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-08-07 23:39:49 +0200

    Update auto_ad_deleter.py

--- HA/auto_ad_deleter.py
+++ HA/auto_ad_deleter.py
@@ -1,4 +1,3 @@
-from datetime import datetime, timedelta
 from discord.ext.commands import Cog, Bot
 from discord.ext import tasks
 
@@ -7,28 +6,17 @@ class timerrule(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
         self._5min_rule.start()
+        self._1hr_rule.start()
 
     @tasks.loop(minutes=5)
     async def _5min_rule(self):
         channel = await self.bot.fetch_channel(1134222661753503775)
-        now = datetime.utcnow()
-        try:
-            async for message in channel.history(limit=None, oldest_first=True):
-                if now - message.created_at > timedelta(minutes=5):
-                    await message.delete(delay=0.5)
-        except:
-            pass
+        await channel.purge(limit=100)
 
     @tasks.loop(hours=1)
     async def _1hr_rule(self):
         channel = await self.bot.fetch_channel(1134226399385890916)
-        now = datetime.utcnow()
-        try:
-            async for message in channel.history(limit=None, oldest_first=True):
-                if now - message.created_at > timedelta(hours=1):
-                    await message.delete(delay=0.5)
-        except:
-            pass
+        await channel.purge(limit=100)
 
 
 async def setup(bot: Bot):
