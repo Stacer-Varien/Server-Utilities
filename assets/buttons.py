3d1722b1426debc075ed37f5c3aa3c312dc6f2b0
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-09-26 00:08:52 +0200

    Update buttons.py

--- assets/buttons.py
+++ assets/buttons.py
@@ -1,13 +1,14 @@
 from typing import Optional
-from discord import User, ui, ButtonStyle, Interaction
+
+from discord import User, ui, ButtonStyle
 from discord.interactions import Interaction
 
 
 class Confirmation(ui.View):
-    def __init__(self, author:Optional[User]=None):
+    def __init__(self, author: Optional[User] = None):
         super().__init__(timeout=600)
         self.value = None
-        self.author=author
+        self.author = author
 
     @ui.button(label="Confirm", style=ButtonStyle.green)
     async def confirm(self, button: ui.Button, ctx: Interaction):
@@ -18,20 +19,21 @@ class Confirmation(ui.View):
     async def cancel(self, button: ui.Button, ctx: Interaction):
         self.value = False
         self.stop()
-    
+
     async def interaction_check(self, ctx: Interaction):
         return ctx.user.id == self.author.id
 
+
 class MobileView(ui.View):
-    def __init__(self, author:Optional[User]=None):
+    def __init__(self, author: Optional[User] = None):
         super().__init__(timeout=600)
         self.value = None
-        self.author=author
+        self.author = author
 
     @ui.button(label="Mobile Version", style=ButtonStyle.green)
     async def confirm(self, button: ui.Button, ctx: Interaction):
         self.value = True
         self.stop()
-    
+
     async def interaction_check(self, ctx: Interaction):
         return ctx.user.id == self.author.id
