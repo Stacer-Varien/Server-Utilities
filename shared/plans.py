cc8833f66d4651a670b44d3421a19eeb5c93211c
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-09-26 00:04:31 +0200

    Update plans.py

--- shared/plans.py
+++ shared/plans.py
@@ -1,12 +1,13 @@
 from datetime import datetime, timedelta
 from random import randint
-from humanfriendly import parse_timespan
+
 from discord import *
 from discord import app_commands as Serverutil
 from discord.ext.commands import GroupCog, Bot
-from config import hazead, loa, lss
+from humanfriendly import parse_timespan
 
 from assets.functions import Plans
+from config import hazead, loa, lss
 
 planmanager = 956634941066739772
 operationmanager = 841671956999045141
@@ -33,7 +34,9 @@ class plancog(GroupCog, name="plan"):
             guild_id = 704888699590279221
             plan_log_channel_id = 990246941029990420
 
-        Plans(guild_id).add(member, round(ending_time.timestamp()), plan, ctx.user, plan_id)
+        Plans(guild_id).add(
+            member, round(ending_time.timestamp()), plan, ctx.user, plan_id
+        )
 
         plan_log_channel = self.bot.get_channel(plan_log_channel_id)
         planned = Embed(title="New Plan", color=Color.blue())
@@ -49,8 +52,8 @@ class plancog(GroupCog, name="plan"):
         )
         await plan_log_channel.send(embed=planned)
         await ctx.followup.send(
-                "Plan added to {}".format(plan_log_channel.mention), ephemeral=True
-            )
+            "Plan added to {}".format(plan_log_channel.mention), ephemeral=True
+        )
 
     @Serverutil.command(description="End a plan if cancelled early")
     @Serverutil.checks.has_any_role(planmanager, operationmanager, loa_staff_team)
@@ -70,6 +73,7 @@ class plancog(GroupCog, name="plan"):
         else:
             plan.remove(plan_id)
             await ctx.followup.send("Plan removed")
+
 
 async def setup(bot: Bot):
     await bot.add_cog(
