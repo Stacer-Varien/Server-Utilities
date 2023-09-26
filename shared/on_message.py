7d981a5f486d11c0a53a8775dd1e906412c80216
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-09-27 00:18:40 +0200

    Update on_message.py

--- shared/on_message.py
+++ shared/on_message.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 from random import randint
 
-from discord import Color, Embed, Message
+from discord import Color, Embed, Message, Forbidden
 from discord.ext.commands import Cog, Bot
 from discord.utils import utcnow
 
@@ -9,8 +9,7 @@ from assets.buttons import Confirmation
 from assets.functions import Warn, LOAMod
 from assets.not_allowed import no_invites, no_nsfw_spam, not_allowed_nsfw
 
-
-class automodcog(Cog):
+class AutomodCog(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
@@ -20,91 +19,88 @@ class automodcog(Cog):
             if message.channel.id in no_invites:
                 if not message.author.bot:
                     await message.delete()
-                    warn_id = f"{randint(0,100000)}"
+                    warn_id = randint(0, 100000)
                     warn_data = Warn(message.author, self.bot.user, warn_id)
 
-                    if warn_data.auto_give(message.channel) == False:
-                        pass
-                    else:
-                        warn_data.auto_give(message.channel)
-                        adwarn_channel = message.guild.get_channel(925790260695281703)
-                        reason = f"Incorrectly advertising in {message.channel.mention}"
+                    if not warn_data.auto_give(message.channel):
+                        return
 
-                        warn_points = warn_data.get_points()
+                    warn_data.auto_give(message.channel)
+                    adwarn_channel = message.guild.get_channel(925790260695281703)
+                    reason = f"Incorrectly advertising in {message.channel.mention}"
 
-                        embed = Embed(title="You have been warned", color=0xFF0000)
-                        embed.add_field(
-                            name="Reason of warn", value=reason, inline=True
+                    warn_points = warn_data.get_points()
+
+                    embed = Embed(title="You have been warned", color=0xFF0000)
+                    embed.add_field(name="Reason of warn", value=reason, inline=True)
+                    embed.add_field(name="Warn ID", value=warn_id, inline=True)
+                    embed.add_field(name="Warn Points", value=warn_points, inline=True)
+
+                    if warn_points == 3:
+                        await message.author.edit(
+                            timed_out_until=(utcnow() + timedelta(hours=2)),
+                            reason="2 hour mute punishment applied",
                         )
-                        embed.add_field(name="Warn ID", value=warn_id, inline=True)
-                        embed.add_field(
-                            name="Warn Points", value=warn_points, inline=True
-                        )
-
-                        if warn_points == 3:
-                            await message.author.edit(
-                                timeout=utcnow() + timedelta(hours=2),
-                                reason="2 hour mute punishment applied",
+                        result = "Member has reached the 3 warn point punishment. A 2 hour mute punishment was applied"
+                        try:
+                            timeoutmsg = Embed(
+                                description=f"You have recieved a timeout of 3 hours from **{message.guild.name}**\nYou have reached the 3 warn point punishment"
                             )
-                            result = "Member has reached the 3 warn point punishment. A 2 hour mute punishment was applied"
-                            try:
-                                timeoutmsg = Embed(
-                                    description=f"You have recieved a timeout of 3 hours from **{message.guild.name}**\nYou have reached the 3 warn point punishment"
-                                )
-                                await message.author.send(embed=timeoutmsg)
-                            except:
-                                pass
+                            await message.author.send(embed=timeoutmsg)
+                        except Forbidden:
+                            pass
 
-                        elif warn_points == 6:
-                            try:
-                                kickmsg = Embed(
-                                    description=f"You are kicked from **{message.guild.name}**\nYou have reached the 6 warn point punishment"
-                                )
-                                await message.author.send(embed=kickmsg)
-                            except:
-                                pass
-                            await message.author.kick(reason="Kick punishment applied")
-                            result = "Member has reached the 6 warn point punishment. A kick punishment was applied"
+                    elif warn_points == 6:
+                        try:
+                            kickmsg = Embed(
+                                description=f"You are kicked from **{message.guild.name}**\nYou have reached the 6 warn point punishment"
+                            )
+                            await message.author.send(embed=kickmsg)
+                        except Forbidden:
+                            pass
+                        await message.author.kick(reason="Kick punishment applied")
+                        result = "Member has reached the 6 warn point punishment. A kick punishment was applied"
 
-                        elif warn_points == 10:
-                            try:
-                                banmsg = Embed(
-                                    description=f"You are banned from **{message.guild.name}**\nYou have reached the 10 warn point punishment"
-                                )
-                                await message.author.send(embed=banmsg)
-                            except:
-                                pass
-                            await message.author.ban(reason="Ban punishment applied")
-                            result = "Member has reached the 10 warn point punishment. A ban punishment was applied"
+                    elif warn_points == 10:
+                        try:
+                            banmsg = Embed(
+                                description=f"You are banned from **{message.guild.name}**\nYou have reached the 10 warn point punishment"
+                            )
+                            await message.author.send(embed=banmsg)
+                        except Forbidden:
+                            pass
+                        await message.author.ban(reason="Ban punishment applied")
+                        result = "Member has reached the 10 warn point punishment. A ban punishment was applied"
 
-                        else:
-                            result = "No warn point punishment applied"
+                    else:
+                        result = "No warn point punishment applied"
 
-                        embed.add_field(name="Result", value=result, inline=False)
-                        embed.set_footer(
-                            text="If you feel this warn was a mistake, please use `/appeal WARN_ID`"
-                        )
-                        embed.set_thumbnail(url=message.author.display_avatar)
-                        await adwarn_channel.send(message.author.mention, embed=embed)
-                else:
-                    pass
-            elif message.channel.id in not_allowed_nsfw:
-                await message.delete()
-            else:
-                pass
-        elif message.channel.id == 1041309643449827360:
+                    embed.add_field(name="Result", value=result, inline=False)
+                    embed.set_footer(
+                        text="If you feel this warn was a mistake, please use `/appeal WARN_ID`"
+                    )
+                    embed.set_thumbnail(url=message.author.display_avatar)
+                    await adwarn_channel.send(message.author.mention, embed=embed)
+                    return
+
+        if message.channel.id in not_allowed_nsfw:
+            await message.delete()
+            return
+
+        if message.channel.id == 1041309643449827360:
             attachments = bool(message.attachments)
             content = bool(message.content)
 
-            if content == True and attachments == False:
-                await message.delete()
-            elif content == True and attachments == True:
-                pass
-            elif attachments == True and content == False:
-                pass
-        elif message.channel.id in no_nsfw_spam:
+        if content is True and attachments is False:
+            await message.delete()
+            return
+        if content is True and attachments is True:
+            return
+        if attachments is True and content is False:
+            return
+        if message.channel.id in no_nsfw_spam:
             attachments = bool(message.attachments)
-            if attachments == True:
+            if attachments is True:
                 if len(message.attachments) > 5:
                     await message.delete()
                     await message.channel.send(
@@ -113,7 +109,8 @@ class automodcog(Cog):
                         ),
                         delete_after=5,
                     )
-        elif message.channel.id == 954594959074418738:
+            return
+        if message.channel.id == 954594959074418738:
             if not message.author.bot:
                 data = LOAMod(message.author)
                 view = Confirmation(message.author)
@@ -143,7 +140,7 @@ class automodcog(Cog):
 
                 await view.wait()
 
-                if view.value == True:
+                if view.value is True:
                     data.add_wick_action_point()
                     embed = Embed(
                         title=f"Wick {action} command used", color=Color.blue()
@@ -167,10 +164,10 @@ class automodcog(Cog):
                     await m.edit(embed=added, view=None, delete_after=5)
                     await log_ch.send(embed=embed)
 
-                elif view.value == False:
+                elif view.value is False:
                     added = Embed(
                         description="Confirmation completed. This message will be deleted.",
-                        color=Color.green(),
+                        color=Color.red(),
                     )
                     await m.edit(embed=added, view=None, delete_after=5)
 
@@ -180,8 +177,8 @@ class automodcog(Cog):
                         color=Color.red(),
                     )
                     await m.edit(embed=added, view=None, delete_after=5)
-                await self.bot.process_commands(message)
+        await self.bot.process_commands(message)
 
 
 async def setup(bot: Bot):
-    await bot.add_cog(automodcog(bot))
+    await bot.add_cog(AutomodCog(bot))
