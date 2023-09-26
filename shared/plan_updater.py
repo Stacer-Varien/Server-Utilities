6235933d8cfd2aba08b8569b0af561066f02f627
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-09-27 00:19:07 +0200

    Update plan_updater.py

--- shared/plan_updater.py
+++ shared/plan_updater.py
@@ -5,7 +5,6 @@ from discord.ext import tasks, commands
 
 from assets.functions import Plans
 
-
 class PlanUpdaterCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
@@ -18,26 +17,25 @@ class PlanUpdaterCog(commands.Cog):
 
         plans = Plans(704888699590279221)
 
-        if plans.check() == None:
-            pass
-        else:
-            for i in plans.check():
-                buyer = await self.bot.fetch_user(int(i[0]))
-                ending = int(i[2])
-                plan = i[3]
-                setter = await self.bot.fetch_user(int(i[4]))
-                plan_id = int(i[5])
+        if plans.check() is None:
+            return
 
-                if int(datetime.now().timestamp()) > ending:
-                    embed = Embed(title="Plan")
-                    embed.add_field(name="Buyer", value=buyer, inline=False)
-                    embed.add_field(name="Product", value=plan, inline=False)
-                    await loa_plan_log.send(
-                        "{}, {} has ended".format(setter.mention, plan_id), embed=embed
-                    )
-                    plans.remove(buyer, plan_id)
-                else:
-                    pass
+        for i in plans.check():
+            buyer = await self.bot.fetch_user(int(i[0]))
+            ending = int(i[2])
+            plan = i[3]
+            setter = await self.bot.fetch_user(int(i[4]))
+            plan_id = int(i[5])
+
+            if int(datetime.now().timestamp()) > ending:
+                embed = Embed(title="Plan")
+                embed.add_field(name="Buyer", value=buyer, inline=False)
+                embed.add_field(name="Product", value=plan, inline=False)
+                await loa_plan_log.send(
+                    "{}, {} has ended".format(setter.mention, plan_id), embed=embed
+                )
+                plans.remove(buyer, plan_id)
+                return
 
     @tasks.loop(minutes=5)
     async def update_plans_ha(self):
@@ -45,28 +43,26 @@ class PlanUpdaterCog(commands.Cog):
 
         plans = Plans(925790259160166460)
 
-        if plans.check() == None:
-            pass
+        if plans.check() is None:
+            return
 
-        else:
-            for i in plans.check():
-                buyer = await self.bot.fetch_user(i[0])
-                ending = i[2]
-                plan = i[3]
-                setter = await self.bot.fetch_user(i[4])
-                plan_id = i[5]
+        for i in plans.check():
+            buyer = await self.bot.fetch_user(i[0])
+            ending = i[2]
+            plan = i[3]
+            setter = await self.bot.fetch_user(i[4])
+            plan_id = i[5]
 
-                if int(round(datetime.now().timestamp())) > int(ending):
-                    embed = Embed(title="Plan")
-                    embed.add_field(name="Buyer", value=buyer, inline=False)
-                    embed.add_field(name="Product", value=plan, inline=False)
-                    embed.add_field(name="Ending", value=ending, inline=False)
-                    await ha_planlog.send(
-                        "{}, {} has ended".format(setter.mention, plan_id), embed=embed
-                    )
-                    plans.remove(buyer, plan_id)
-                else:
-                    pass
+            if int(round(datetime.now().timestamp())) > int(ending):
+                embed = Embed(title="Plan")
+                embed.add_field(name="Buyer", value=buyer, inline=False)
+                embed.add_field(name="Product", value=plan, inline=False)
+                embed.add_field(name="Ending", value=ending, inline=False)
+                await ha_planlog.send(
+                    "{}, {} has ended".format(setter.mention, plan_id), embed=embed
+                )
+                plans.remove(buyer, plan_id)
+                return
 
     @update_plans_loa.before_loop
     async def before_update_plans_loa(self):
