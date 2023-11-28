81de7d2096963115eb345121681aa987e1d18f82
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2023-11-28 21:18:57 +0200

    Update error.py

--- shared/error.py
+++ shared/error.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-from discord import Color, Embed, Interaction
+from discord import Color, Embed, File, Interaction
 from discord import app_commands as Serverutil
 from discord.ext.commands import Bot, Cog, Context, NotOwner, CommandNotFound
 import traceback
-
+from io import BytesIO
 
 class ErrorsCog(Cog):
     def __init__(self, bot: Bot):
@@ -38,10 +38,17 @@ class ErrorsCog(Cog):
             traceback_error = traceback.format_exception(
                 error, error, error.__traceback__
             )
+
+            trace="".join(traceback_error)
+            
             with open("errors.txt", "a") as f:
-                f.writelines(f"{datetime.now()} --- {''.join(traceback_error)}")
+                f.writelines(f"{datetime.now()} --- {trace}")
+            
+            file=BytesIO(trace.encode("utf-8"))
+            file=File(file, filename="error.txt")
+            
             channel = await self.bot.fetch_channel(1169726260533018644)
-            await channel.send(traceback_error)
+            await channel.send(file=file)
         elif isinstance(error, Serverutil.errors.NoPrivateMessage):
             embed = Embed(description=str(error), color=Color.red())
 
