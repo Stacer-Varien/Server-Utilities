11b8969a96f398e7c5e5e2b4c2523239bfb4484a
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2024-01-25 16:57:35 +0200

    Update

--- VHF/selfroles.py
+++ VHF/selfroles.py
@@ -1,49 +1,66 @@
-from discord import ButtonStyle, Color, Embed, Emoji, PartialEmoji, ui, app_commands as ServerUtils
+from discord import (
+    ButtonStyle,
+    Color,
+    Embed,
+    Emoji,
+    Message,
+    Object,
+    RawReactionActionEvent,
+    ui,
+)
 from discord.ext.commands import Bot, Cog
 from discord.ext import tasks
 from asyncio import sleep
+from assets.buttons import EventRolesButtons
 
 
 class SelfRolesCog(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
-        self.eventroles.start()
 
-        self.buttons = {
-            "pride": self.create_button("Pride Month", "🌈"),
-            "halloween": self.create_button("Halloween", "🎃"),
-            "elf": self.create_button("Elf Week", "🧝"),
-        }
+    pride_button = ui.View().add_item(
+        ui.Button(
+            style=ButtonStyle.green,
+            label="Pride Month",
+            emoji="🌈",
+            custom_id="pride",
+        )
+    )
 
-    def create_button(self, label:(str|None), emoji:(str | Emoji | PartialEmoji | None)):
-        return ui.View().add_item(
-            ui.Button(
-                style=ButtonStyle.green,
-                label=label,
-                emoji=emoji,
-                custom_id=label.lower()),
-            )
-        
+    halloween_button = ui.View().add_item(
+        ui.Button(
+            style=ButtonStyle.green,
+            label="Halloween",
+            emoji="🎃",
+        )
+    )
+
+    elf_button = ui.View().add_item(
+        ui.Button(
+            style=ButtonStyle.green,
+            label="Elf Week",
+            emoji="🧝",
+            custom_id="elf",
+        )
+    )
 
     @tasks.loop(hours=1)
-    async def eventroles(self):
-        guild_id = 974028573893595146
-        eventroles_channel_id = 1115725010649235608
+    async def selfroles(self):
+        view=EventRolesButtons()
+        vhf = await self.bot.fetch_guild(974028573893595146)
+        eventroles_channel = await vhf.fetch_channel(1115725010649235608)
+        pride = await eventroles_channel.fetch_message(1199715720146210846)
+        halloween = await eventroles_channel.fetch_message(1199718222518296616)
+        elf = await eventroles_channel.fetch_message(1199719535566143608)
 
-        guild = await self.bot.fetch_guild(guild_id)
-        eventroles_channel = await guild.fetch_channel(eventroles_channel_id)
+        await pride.edit(view=view.pride)
+        await sleep(5)
+        await halloween.edit(view=view.halloween)
+        await sleep(5)
+        await elf.edit(view=view.elf)
+        await sleep(5)
 
-        for button_id, message_id in [
-            ("pride", 1199715720146210846),
-            ("halloween", 1199718222518296616),
-            ("elf", 1199719535566143608),
-        ]:
-            message = await eventroles_channel.fetch_message(message_id)
-            await message.edit(view=self.buttons[button_id])
-            await sleep(5)
-    
-    @ServerUtils.command("Di")
 
 
 async def setup(bot: Bot):
-    await bot.add_cog(SelfRolesCog(bot))
+    await bot.add_cog(SelfRolesCog(bot), guild=Object(974028573893595146))
