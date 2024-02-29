6039179272e89877bb0a14420eb8e1772422dd0c
Gary Jordaan <82768608+Stacer-Varien@users.noreply.github.com>
2024-02-29 23:23:20 +0200

    Commit

--- assets/receipt_generator/generator.py
+++ assets/receipt_generator/generator.py
@@ -2,7 +2,6 @@ from io import BytesIO
 import os
 from random import randint
 from typing import Optional, List
-from PIL import Image, ImageDraw, ImageFont
 from discord import Interaction, Member
 from datetime import datetime
 from assets.functions import Currency
@@ -10,265 +9,218 @@ from assets.functions import Currency
 
 class ReceiptGenerator:
     def __init__(self) -> None:
-        self.font = os.path.join(os.path.dirname(__file__), "absender1.ttf")
+        pass
 
     async def generate_receipt(
-        self, ctx: Interaction, type: str, **kwargs
+        self,
+        ctx: Interaction,
+        type: str,
+        tier: str,
+        days: Optional[int] = None,
+        winners: Optional[int] = 1,
+        custom_prize: Optional[str] = None,
+        use_alt_link: Optional[bool] = False,
+        ping: Optional[str] = "Giveaway Ping",
+        servers:Optional[str]=None
     ) -> BytesIO:
         bank_instance = Currency(ctx.user)
-        receipt_template = os.path.join(os.path.dirname(__file__), "autoad.png")
+        main_template = f"""
+HAZE Advertising HAZE Coins Receipt
+=====
+Customer: {ctx.user}
+Customer ID: {ctx.user.id}
+Date of Purchase: {datetime.now().strftime("%d/%m/%Y %H:%M")}
+"""
         if type == "Giveaway":
-            receipt_template = os.path.join(
-                os.path.dirname(__file__), "assets", "receipt_generator", "giveaway.png"
-            )
-        elif type == "Premium":
-            receipt_template = os.path.join(
-                os.path.dirname(__file__), "assets", "receipt_generator", "premium.png"
-            )
-        elif type == "SpecialServers":
-            receipt_template = os.path.join(
-                os.path.dirname(__file__),
-                "assets",
-                "receipt_generator",
-                "specialservers.png",
-            )
-
-        rt_open = Image.open(receipt_template).convert("RGBA")
-        draw = ImageDraw.Draw(rt_open)
-
-
-        draw.text(
-            (640, 540),
-            text=ctx.user.name,
-            fill=(0, 0, 0),
-            font=ImageFont.truetype(self.font, 60),
-        )
-        draw.text(
-            (640, 700),
-            text=str(ctx.user.id),
-            fill=(0, 0, 0),
-            font=ImageFont.truetype(self.font, 60),
-        )
-        draw.text(
-            (640, 820),
-            text=datetime.now().strftime("%d/%m%Y"),
-            fill=(0, 0, 0),
-            font=ImageFont.truetype(self.font, 60),
-        )
-
-        if type == "Autoad":
-            draw.text(
-                (1260, 290),
-                text=str(randint(0, 99999)),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-
-            draw.text(
-                (640, 950),
-                text=kwargs.get("product"),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-            draw.text(
-                (640, 1110),
-                text=str(kwargs.get("custom_webhook")),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-            draw.text(
-                (640, 1170),
-                text=str(kwargs.get("days")),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-            draw.text(
-                (640, 1260),
-                text="\n".join(kwargs.get("channels", [])),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-
-
-            total_cost = self.calculate_autoad_cost(kwargs)
-            if bank_instance.get_balance < total_cost:
-                await ctx.edit_original_response(
-                    "Your balance is too low. Please try again when you have sufficient funds"
+            if tier == "Tier 1":
+                cost = 1000
+                prize = (
+                    custom_prize if custom_prize != None else "Premium Role for 10 days"
                 )
-                return
-
-            draw.text(
-                (870, 2030),
-                text=str(total_cost),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-        elif type == "Giveaway":
-
-            draw.text(
-                (1260, 290),
-                text=str(randint(0, 99999)),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-            draw.text(
-                (640, 950),
-                text=kwargs.get("product"),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-            draw.text(
-                (640, 1120),
-                text=str(kwargs.get("winners")),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-            draw.text(
-                (640, 1210),
-                text="Yes",
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-            draw.text(
-                (640, 1300),
-                text=str(kwargs.get("use_of_alt_link")),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-            draw.text(
-                (640, 1370),
-                text=str(kwargs.get("use_of_pings")),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-            draw.text(
-                (640, 1460),
-                text="\n".join(kwargs.get("prizes", [])),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-
-
-            total_cost = self.calculate_giveaway_cost(kwargs)
-            draw.text(
-                (1040, 1850),
-                text=total_cost,
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-        elif type == "Premium":
-            draw.text(
-                (1260, 290),
-                text=str(randint(0, 99999)),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-            total_cost = 1500
-            if bank_instance.get_balance < total_cost:
-                await ctx.edit_original_response(
-                    "Your balance is too low. Please try again when you have sufficient funds"
+                cost = cost + (
+                    len(custom_prize.split(",") * 50) if custom_prize != None else 0
                 )
-                return
-            draw.text(
-                (1080, 1000),
-                text=str(total_cost),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
+                days = days if (days != None) else 3
+                cost = cost + ((days - 3) * 50)
+                can_use_alternative_link = "No" if use_alt_link == False else "Yes"
+                cost = cost + (50 if use_alt_link == True else 0)
+                pings = (
+                    "Everyone"
+                    if ping == "Everyone"
+                    else "Here" if ping == "Here" else None
+                )
+                cost = cost + (
+                    200 if ping == "Everyone" else 150 if ping == "Here" else 0
+                )
+
+            elif tier == "Tier 2":
+                cost = 1250
+                prize = (
+                    custom_prize
+                    if custom_prize != None
+                    else "Shoutout with Shoutout Ping"
+                )
+                cost = cost + (
+                    len(custom_prize.split(",") * 50) if custom_prize != None else 0
+                )
+                days = days if days != None else 3
+                cost = cost + ((days - 3) * 50)
+                can_use_alternative_link = "Yes" 
+                pings = (
+                    "Everyone"
+                    if ping == "Everyone"
+                    else "Here" if ping == "Here" else None
+                )
+                cost = cost + (
+                    200 if ping == "Everyone" else 150 if ping == "Here" else 0
+                )
+
+            elif tier == "Tier 3":
+                cost = 1500
+                prize = (
+                    custom_prize
+                    if custom_prize != None
+                    else " Autoad per 5 hours in ⁠♾🔄-unlimited🔄♾  for 7 days"
+                )
+                cost = cost + (
+                    len(custom_prize.split(",") * 50) if custom_prize != None else 0
+                )
+                days = days if days != None else 4
+                cost = cost + ((days - 4) * 50)
+                can_use_alternative_link = "Yes"
+                pings = (
+                    "Everyone"
+                    if ping == "Everyone"
+                    else "Here" if ping == "Here" else None
+                )
+                cost = cost + (
+                    200 if ping == "Everyone" else 150 if ping == "Here" else 0
+                )
+
+            products_template = f"""
+Product description:
+• {winners} winner(s)
+• Prize: {prize}
+• Ping: {ping}
+• Duration: {days} days
+• Required to join your server: Yes
+• Can use social links as alternative: {can_use_alternative_link}
+
+TOTAL COST = {cost} HAZE Coins
+=====
+Thank you for buying from HAZE Advertising with your coins. Please wait up to 24 to 36 hours to have your service delivered.
+
+        © 2024 HAZE Advertising
+"""
+        elif type == "Premium":
+            cost=1500
+            products_template = """
+Product description: 
+• Premium Role for life
+
+TOTAL COST = 1500 HAZE Coins
+=====
+Thank you for buying from HAZE Advertising with your coins. Please wait up to 24 to 36 hours to have your service delivered.
+
+        © 2024 HAZE Advertising
+"""
         elif type == "SpecialServers":
-            draw.text(
-                (1260, 290),
-                text=str(randint(0, 99999)),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-            draw.text(
-                (640, 1000),
-                text=str(kwargs.get("days")),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
-            draw.text(
-                (640, 1130),
-                text=str(len(kwargs.get("servers", "").split(","))),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
+            cost=1000
+            if len(servers.split()) > 1:
+                cost = cost + ((len(servers.split()) - 1) * 50)
+            days= days if days != None else 30
+            cost=cost +((days -30) * 50)
 
+            products_template = f"""
+Product description:
+⁠• Server will be posted in 👋👤joins-and-leaves👤👋 when someone joins
+• Servers: {servers}
+• Days: {days} days
 
-            total_cost = self.calculate_special_servers_cost(kwargs)
-            draw.text(
-                (970, 1255),
-                text=str(total_cost),
-                fill=(0, 0, 0),
-                font=ImageFont.truetype(self.font, 60),
-            )
+TOTAL COST = {cost} HAZE Coins
+=====
+Thank you for buying from HAZE Advertising with your coins. Please wait up to 24 to 36 hours to have your service delivered.
 
-        final_bytes = BytesIO()
-        rt_open.save(final_bytes, "png")
-        final_bytes.seek(0)
-        await bank_instance.remove_credits(int(total_cost))
-        return final_bytes
+        © 2024 HAZE Advertising
+"""
+        elif type == "Autoad":
+            if tier == "Tier 1":
+                cost = 1000
+                prize = (
+                    custom_prize if custom_prize != None else "Premium Role for 10 days"
+                )
+                cost = cost + (
+                    len(custom_prize.split(",") * 50) if custom_prize != None else 0
+                )
+                days = days if (days != None) else 3
+                cost = cost + ((days - 3) * 50)
+                can_use_alternative_link = "No" if use_alt_link == False else "Yes"
+                cost = cost + (50 if use_alt_link == True else 0)
+                pings = (
+                    "Everyone"
+                    if ping == "Everyone"
+                    else "Here" if ping == "Here" else None
+                )
+                cost = cost + (
+                    200 if ping == "Everyone" else 150 if ping == "Here" else 0
+                )
 
-    def calculate_autoad_cost(self, kwargs: dict) -> int:
-        product = kwargs.get("product")
-        days = kwargs.get("days")
-        channels = kwargs.get("channels", [])
-        custom_webhook = kwargs.get("custom_webhook")
+            elif tier == "Tier 2":
+                cost = 1250
+                prize = (
+                    custom_prize
+                    if custom_prize != None
+                    else "Shoutout with Shoutout Ping"
+                )
+                cost = cost + (
+                    len(custom_prize.split(",") * 50) if custom_prize != None else 0
+                )
+                days = days if days != None else 3
+                cost = cost + ((days - 3) * 50)
+                can_use_alternative_link = "Yes" 
+                pings = (
+                    "Everyone"
+                    if ping == "Everyone"
+                    else "Here" if ping == "Here" else None
+                )
+                cost = cost + (
+                    200 if ping == "Everyone" else 150 if ping == "Here" else 0
+                )
 
-        if product == "Tier 1":
-            prdt = 1000
-        elif product == "Tier 2":
-            prdt = 1500
-        elif product == "Tier 3":
-            prdt = 2000
-        elif product == "Tier 4":
-            prdt = 2500
+            elif tier == "Tier 3":
+                cost = 1500
+                prize = (
+                    custom_prize
+                    if custom_prize != None
+                    else " Autoad per 5 hours in ⁠♾🔄-unlimited🔄♾  for 7 days"
+                )
+                cost = cost + (
+                    len(custom_prize.split(",") * 50) if custom_prize != None else 0
+                )
+                days = days if days != None else 4
+                cost = cost + ((days - 4) * 50)
+                can_use_alternative_link = "Yes"
+                pings = (
+                    "Everyone"
+                    if ping == "Everyone"
+                    else "Here" if ping == "Here" else None
+                )
+                cost = cost + (
+                    200 if ping == "Everyone" else 150 if ping == "Here" else 0
+                )
 
-        duration = 200 * (days - 7)
-        customwebhook = 200 if custom_webhook else 0
-        channels_cost = (
-            0
-            if not channels or channels[0] != "♾🔄-unlimited🔄♾"
-            else (len(channels) - 1) * 200
-        )
+            products_template = f"""
+Product description:
+• {winners} winner(s)
+• Prize: {prize}
+• Ping: {ping}
+• Duration: {days} days
+• Required to join your server: Yes
+• Can use social links as alternative: {can_use_alternative_link}
 
-        return prdt + duration + customwebhook + channels_cost
+TOTAL COST = {cost} HAZE Coins
+=====
+Thank you for buying from HAZE Advertising with your coins. Please wait up to 24 to 36 hours to have your service delivered.
 
-    def calculate_giveaway_cost(self, kwargs: dict) -> int:
-        product = kwargs.get("product")
-        winners = kwargs.get("winners")
-        days = kwargs.get("days")
-        prizes = kwargs.get("prizes", [])
-        use_of_alt_link = kwargs.get("use_of_alt_link")
-        use_of_pings = kwargs.get("use_of_pings")
-
-        if product == "Tier 1":
-            prdt = 1000
-        elif product == "Tier 2":
-            prdt = 1250
-        elif product == "Tier 3":
-            prdt = 1500
-
-        winners_cost = (winners - 1) * 50
-        days_cost = (days - 3) * 50
-        prizes_cost = 0 if not prizes or prizes[0] == prizes else (len(prizes) * 150)
-        alt_link_cost = 100 if use_of_alt_link else 0
-        pings_cost = (
-            150
-            if use_of_pings == "Here"
-            else (200 if use_of_pings == "Everyone" else 0)
-        )
-
-        return (
-            prdt + winners_cost + days_cost + prizes_cost + alt_link_cost + pings_cost
-        )
-
-    def calculate_special_servers_cost(self, kwargs: dict) -> int:
-        days = kwargs.get("days")
-        servers = kwargs.get("servers", "")
-
-        days_cost = (days - 30) * 50
-        servers_cost = (len(servers.split(",")) - 1) * 50
-
-        return 1000 + days_cost + servers_cost
+        © 2024 HAZE Advertising
+"""
